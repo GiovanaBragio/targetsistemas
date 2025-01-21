3794dabe66a51257bb9a0fe1c7e8d60df176130f
Giovana Bragio <giovanacaroba@hotmail.com>
2025-01-21 16:03:58 -0300

    Create primeira_questão.ts

---
+++ primeira_questão.ts
@@ -0,0 +1,15 @@
+let indice: number = 13;
+let SOMA = 0;
+let k = 0;
+
+function soma (){
+    while(k<indice){
+        k = k +1;
+        SOMA = SOMA + k;
+        console.log(SOMA)
+    }
+}
+
+soma();
+
+//o resultado da soma é 91
